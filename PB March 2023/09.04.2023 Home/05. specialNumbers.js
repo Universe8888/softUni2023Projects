676ab7ea48e4f2a3adf38c44df264923ddd7a570
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-04-12 18:27:57 +0300

    Update 05. specialNumbers.js

--- PB March 2023/09.04.2023 Home/05. specialNumbers.js
+++ PB March 2023/09.04.2023 Home/05. specialNumbers.js
@@ -1,9 +1,16 @@
+//withouth MAP
+
 function specialNumbers(input) {
     let N = parseInt(input[0]);
     let specialNumbers = [];
 
     for (let i = 1111; i <= 9999; i++) {
-        let digits = i.toString().split('').map(Number);
+        let d1 = Math.floor(i / 1000);
+        let d2 = Math.floor((i % 1000) / 100);
+        let d3 = Math.floor((i % 100) / 10);
+        let d4 = i % 10;
+        let digits = [d1, d2, d3, d4];
+        
         let isSpecial = true;
 
         for (let digit of digits) {
@@ -22,3 +29,31 @@ function specialNumbers(input) {
 }
 
 specialNumbers(["3"]);
+
+
+
+
+// function specialNumbers(input) {
+//     let N = parseInt(input[0]);
+//     let specialNumbers = [];
+
+//     for (let i = 1111; i <= 9999; i++) {
+//         let digits = i.toString().split('').map(Number);
+//         let isSpecial = true;
+
+//         for (let digit of digits) {
+//             if (digit === 0 || N % digit !== 0) {
+//                 isSpecial = false;
+//                 break;
+//             }
+//         }
+
+//         if (isSpecial) {
+//             specialNumbers.push(i);
+//         }
+//     }
+
+//     console.log(specialNumbers.join(' '));
+// }
+
+// specialNumbers(["3"]);
