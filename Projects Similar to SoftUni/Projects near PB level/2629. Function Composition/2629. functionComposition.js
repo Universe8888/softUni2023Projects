77a3d6135ecac5515226058c9fc7824e5bcb3f6e
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-04-20 20:33:57 +0300

    Update 2629. functionComposition.js

--- Projects Similar to SoftUni/Projects near PB level/2629. Function Composition/2629. functionComposition.js
+++ Projects Similar to SoftUni/Projects near PB level/2629. Function Composition/2629. functionComposition.js
@@ -4,9 +4,18 @@
  */
 var compose = function(functions) {
 	return function(x) {
-        
+        return functions.reduceRight((value, func) => func(value), x);
     }
 };
+
+const fn = compose([x => x + 1, x => x * x, x => 2 * x]);
+console.log(fn(4));
+
+const fn2 = compose([x => 10 * x, x => 10 * x, x => 10 * x]);
+console.log(fn2(1));
+
+const fn3 = compose([]);
+console.log(fn3(42)); 
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
