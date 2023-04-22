27dd476ff9b72e72daa27c7241d87167e56632ed
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-04-22 12:53:59 +0300

    Update 01. catDiet.js

--- PB March 2023/Programming Basics Online Pre - Exam - 22 and 23 April 2023/01. catDiet.js
+++ PB March 2023/Programming Basics Online Pre - Exam - 22 and 23 April 2023/01. catDiet.js
@@ -1,22 +1,28 @@
 function catDiet(input) {
-    let fatsPercent = Number(input[0]);
+    let fatPercent = Number(input[0]);
     let proteinPercent = Number(input[1]);
     let carbPercent = Number(input[2]);
     let totalCalories = Number(input[3]);
     let waterPercent = Number(input[4]);
-
-    let fatGrams = (fatsPercent * totalCalories) / 9;
-    let proteinGrams = (proteinPercent * totalCalories) / 4;
-    let carbGrams = (carbPercent * totalCalories) / 4;
-
-    let foodWeight = fatGrams + proteinGrams + carbGrams;
-    let wetFoodWeight = foodWeight / (1 - waterPercent / 100);
-    let dryFoodWeight = wetFoodWeight * (1 - waterPercent / 100);
-
-    let caloriesPerGram = totalCalories / foodWeight;
-    let nonWaterCaloriesPerGram = caloriesPerGram / (1 - waterPercent / 100);
-
-    return nonWaterCaloriesPerGram.toFixed(4);
-}
-
-console.log(catDiet(["60", "25", "15", "2500", "60"]));
+  
+    // Calculate grams of macronutrients
+    let fatGrams = (fatPercent/100) * totalCalories / 9;
+    let proteinGrams = (proteinPercent/100) * totalCalories / 4;
+    let carbGrams = (carbPercent/100) * totalCalories / 4;
+  
+    // Calculate total food weight
+    let totalWeight = fatGrams + proteinGrams + carbGrams;
+  
+    let caloriesPerOnceGramFood = totalCalories /totalWeight;
+  
+    let dryFood = (1 - (waterPercent/100)) / ((fatPercent/900) + (proteinPercent/400) + (carbPercent/400));
+  
+    console.log(dryFood.toFixed(4));
+  }
+  
+  catDiet(["60",
+    "25",
+    "15",
+    "2500",
+    "60"]);
+  
