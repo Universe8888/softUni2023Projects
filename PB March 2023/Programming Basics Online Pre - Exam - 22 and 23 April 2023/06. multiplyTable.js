c46ba2dde2f11dff3e9781e7b80e1dc33b5e564e
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-04-22 12:19:19 +0300

    Create 06. multiplyTable.js

---
+++ PB March 2023/Programming Basics Online Pre - Exam - 22 and 23 April 2023/06. multiplyTable.js
@@ -0,0 +1,17 @@
+function multiplyTable(input) {
+    const num = input[0];
+    const firstDigit = parseInt(num[2]);
+    const secondDigit = parseInt(num[1]);
+    const thirdDigit = parseInt(num[0]);
+  
+    for (let i = 1; i <= firstDigit; i++) {
+      for (let j = 1; j <= secondDigit; j++) {
+        for (let k = 1; k <= thirdDigit; k++) {
+          const result = i * j * k;
+          console.log(`${i} * ${j} * ${k} = ${result};`);
+        }
+      }
+    }
+}
+
+multiplyTable(["324"])
