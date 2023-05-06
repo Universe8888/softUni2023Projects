325a189025c65fddbf64ca88d7fe994b9032b45b
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-05-06 16:29:04 +0300

    Update 06. theatrePromotions.js

--- projectsInsideSoftUniPreFundamentals/Basic Syntax, Conditional Statements and Loops - Lab/06. theatrePromotions.js
+++ projectsInsideSoftUniPreFundamentals/Basic Syntax, Conditional Statements and Loops - Lab/06. theatrePromotions.js
@@ -1,4 +1,48 @@
-function theatrePromotions() {
+function theatrePromotions(dayType, age) {
+    let ticketPrice;
 
-    
+    if (age >= 0 && age <= 18) {
+        switch (dayType) {
+            case 'Weekday':
+                ticketPrice = 12;
+                break;
+            case 'Weekend':
+                ticketPrice = 15;
+                break;
+            case 'Holiday':
+                ticketPrice = 5;
+                break;
+        }
+    } else if (age > 18 && age <= 64) {
+        switch (dayType) {
+            case 'Weekday':
+                ticketPrice = 18;
+                break;
+            case 'Weekend':
+                ticketPrice = 20;
+                break;
+            case 'Holiday':
+                ticketPrice = 12;
+                break;
+        }
+    } else if (age > 64 && age <= 122) {
+        switch (dayType) {
+            case 'Weekday':
+                ticketPrice = 12;
+                break;
+            case 'Weekend':
+                ticketPrice = 15;
+                break;
+            case 'Holiday':
+                ticketPrice = 10;
+                break;
+        }
+    } else {
+        console.log("Error!");
+        return;
+    }
+
+    console.log(ticketPrice + "$");
 }
+
+theatrePromotions('Weekday', 42);
