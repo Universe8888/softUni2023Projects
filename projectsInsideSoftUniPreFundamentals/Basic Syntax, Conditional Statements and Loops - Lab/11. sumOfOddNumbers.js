3a2807a21a748e67fa0c8e89324127c8265b272c
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-05-06 16:57:19 +0300

    Create 11. sumOfOddNumbers.js

---
+++ projectsInsideSoftUniPreFundamentals/Basic Syntax, Conditional Statements and Loops - Lab/11. sumOfOddNumbers.js
@@ -0,0 +1,12 @@
+function sumOfOddNumbers(n) {
+    let sum = 0;
+
+    for (let i = 1, odd = 1; i <= n; i++, odd += 2) {
+        console.log(odd);
+        sum += odd;
+    }
+
+    console.log("Sum: " + sum);
+}
+
+sumOfOddNumbers(5);
