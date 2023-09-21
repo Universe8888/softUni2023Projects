0ac067bca9c3740b9c48922c62627f18b4b8ff95
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-09-21 21:30:01 +0300

    Update centuriesToMinutes.js

--- Programming Fundamentals with JavaScript/09.20.2023 Lab/centuriesToMinutes.js
+++ Programming Fundamentals with JavaScript/09.20.2023 Lab/centuriesToMinutes.js
@@ -1,17 +1,10 @@
 function convertCenturies(centuries) {
-    // 1 century = 100 years
-    let years = centuries * 100;
-    
-    // 1 year = 365.24 days
-    let days = Math.floor(years * 365.24);
-    
-    // 1 day = 24 hours
-    let hours = days * 24;
-    
-    // 1 hour = 60 minutes
-    let minutes = hours * 60;
+    const years = centuries * 100; 
+    const days = Math.trunc(years * 365.2422); //added as in example 365.2422
+    const hours = days * 24; 
+    const minutes = hours * 60; 
     
     console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`);
 }
 
-convertCenturies(1);
+convertCenturies(5);
