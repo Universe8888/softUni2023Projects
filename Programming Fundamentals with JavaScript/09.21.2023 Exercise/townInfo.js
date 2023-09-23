ad29d53b977ca05458e7a58cd0ee5c73900b85f0
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-09-23 15:35:33 +0300

    Create townInfo.js

---
+++ Programming Fundamentals with JavaScript/09.21.2023 Exercise/townInfo.js
@@ -0,0 +1,5 @@
+function printTownInfo(townName, population, area) {
+    console.log(`Town ${townName} has population of ${population} and area ${area} square km.`);
+}
+
+printTownInfo('Sofia', 1286383, 492)
