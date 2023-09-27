34c055b207503a410330d069c18b2241a3ea149f
Boris Manzov <borismanzov@outlook.com>
2023-09-27 13:09:07 +0300

    Update townInfo.js

--- Programming Fundamentals with JavaScript/09.21.2023 Exercise/townInfo.js
+++ Programming Fundamentals with JavaScript/09.21.2023 Exercise/townInfo.js
@@ -2,4 +2,4 @@ function printTownInfo(townName, population, area) {
     console.log(`Town ${townName} has population of ${population} and area ${area} square km.`);
 }
 
-printTownInfo('Sofia', 1286383, 492)
+printTownInfo('Sofia', 1286383, 492);
