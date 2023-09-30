2ce1a511519d0b8a6efc213bd116254cccfa0b03
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-09-30 11:43:35 +0300

    Create mergeArrays.js

---
+++ Programming Fundamentals with JavaScript/09.29.2023 Exercise/mergeArrays.js
@@ -0,0 +1,17 @@
+function mergeArrays(arr1, arr2) {
+    let resultArr = [];
+
+    for(let i = 0; i < arr1.length; i++) {
+        if(i % 2 === 0) {
+            resultArr.push(Number(arr1[i]) + Number(arr2[i]));
+        } else {
+            resultArr.push(arr1[i] + arr2[i]);
+        }
+    }
+
+    console.log(resultArr.join(' - '));
+}
+
+mergeArrays(['5', '15', '23', '56', '35'],
+
+['17', '22', '87', '36', '11']);
