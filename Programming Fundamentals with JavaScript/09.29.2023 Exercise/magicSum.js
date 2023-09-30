6404c115da1540a0b73981a8577bbfc529d07313
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-09-30 12:23:35 +0300

    Update magicSum.js

--- Programming Fundamentals with JavaScript/09.29.2023 Exercise/magicSum.js
+++ Programming Fundamentals with JavaScript/09.29.2023 Exercise/magicSum.js
@@ -1,12 +1,27 @@
-function printUniquePairs(arr, sum) {
-    let seen = new Set();
+// function printUniquePairs(arr, sum) {
+//     let pairs = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             if (arr[i] + arr[j] === sum) {
+//                 let pair = [arr[i], arr[j]];
+//                 if (!pairs.some(p => p.includes(arr[i]) && p.includes(arr[j]))) {
+//                     pairs.push(pair);
+//                 }
+//             }
+//         }
+//     }
+//     pairs.forEach(pair => console.log(pair.join(' ')));
+// }
 
-    for (let i = 0; i < arr.length - 1; i++) {
+//Judge did not approve :)
+
+//Using a different basic function
+
+function printUniquePairs(arr, sum) {
+    for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] + arr[j] === sum && !seen.has(arr[i]) && !seen.has(arr[j])) {
-                console.log(`${arr[i]} ${arr[j]}`);
-                seen.add(arr[i]);
-                seen.add(arr[j]);
+            if (arr[i] + arr[j] === sum) {
+                console.log(arr[i] + ' ' + arr[j]);
             }
         }
     }
