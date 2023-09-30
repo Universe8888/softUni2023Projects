1cf1d6537a3037550ff75c841e0c879ea1396dcc
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-09-30 12:02:18 +0300

    Create equalSums.js

---
+++ Programming Fundamentals with JavaScript/09.29.2023 Exercise/equalSums.js
@@ -0,0 +1,22 @@
+function equilibriumIndex(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let leftSum = 0;
+        let rightSum = 0;
+        
+        for (let j = 0; j < i; j++) {
+            leftSum += arr[j];
+        }
+        
+        for (let k = i + 1; k < arr.length; k++) {
+            rightSum += arr[k];
+        }
+        
+        if (leftSum === rightSum) {
+            console.log(i);
+            return;
+        }
+    }
+    console.log('no');
+}
+
+equilibriumIndex([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
