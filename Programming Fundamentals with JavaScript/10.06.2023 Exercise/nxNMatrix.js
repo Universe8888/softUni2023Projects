873606cbbad110ed82166125a4aa46e802169a94
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-07 11:41:23 +0300

    Create nxNMatrix.js

---
+++ Programming Fundamentals with JavaScript/10.06.2023 Exercise/nxNMatrix.js
@@ -0,0 +1,19 @@
+function printMatrix(n) {
+    
+    for (let i = 0; i < n; i++) {
+        
+        let row = '';
+        
+        
+        for (let j = 0; j < n; j++) {
+            
+            row += n + ' ';
+        }
+        
+        
+        console.log(row /n);
+    }
+}
+
+
+printMatrix(3);
