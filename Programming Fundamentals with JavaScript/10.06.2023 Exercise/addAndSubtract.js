cafff16de7b2f4004e42a695efb602687054e776
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-07 11:11:29 +0300

    Update addAndSubtract.js

---
+++ Programming Fundamentals with JavaScript/10.06.2023 Exercise/addAndSubtract.js
@@ -0,0 +1,15 @@
+function addAndSubtract(numOne, numTwo, numThree) {
+    let sumResult = sum(numOne, numTwo);
+    let finalResult = subtract(sumResult, numThree);
+    console.log(finalResult);
+}
+
+function sum(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+addAndSubtract(23, 6, 10);
