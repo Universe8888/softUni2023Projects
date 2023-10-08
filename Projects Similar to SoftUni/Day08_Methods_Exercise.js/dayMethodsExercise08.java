81bf8dfc1f5f466d1c68f76f72f8d040878d18d1
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-08 16:44:31 +0300

    Refactor password validation logic in P04_Password_Validator
    Broke down the validation logic into separate methods (isLengthValid, hasOnlyLettersAndDigits, hasAtLeastTwoDigits) to enhance code readability and maintainability.
    
    Replaced ASCII value checks with Character class methods for more intuitive character validation.
    
    Added user prompts for input to improve interactivity and user experience.
    
    Resource Management: Ensured proper closure of Scanner object to prevent potential resource leaks.

--- Projects Similar to SoftUni/Day08_Methods_Exercise.js/dayMethodsExercise08.java
+++ Projects Similar to SoftUni/Day08_Methods_Exercise.js/dayMethodsExercise08.java
@@ -3,49 +3,53 @@ package Day08_Methods_Exercise;
 import java.util.Scanner;
 
 public class P04_Password_Validator {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter password:");
         String input = scanner.nextLine();
+        scanner.close();
 
-        boolean isValid = true;
-        boolean isValidDigits = true;
-        int digitsCounter = 0;
-
-        for (int i = 0; i < input.length(); i++) {
-            char currentChar = input.charAt(i);
-
-
-            if (currentChar >= 48 && currentChar <= 57) {
-                digitsCounter++;
-            }
-
-            if (currentChar < 48 || currentChar > 57 &&
-                    currentChar < 65 || currentChar > 90 &&
-                    currentChar < 97 || currentChar > 122) {
-                isValidDigits = false;
-                isValid = false;
-            }
-        }
-
-        if ( input.length() > 10 || input.length() < 6) {
-            isValid = false;
-            System.out.println("Password must be between 6 and 10 characters");
-        }
-        if (!isValidDigits) {
-            isValid = false;
-            System.out.println("Password must consist only of letters and digits");
-        }
-        if (digitsCounter < 2) {
-            System.out.println("Password must have at least 2 digits");
-            isValid = false;
-        }
-        if (isValid) {
+        if (isLengthValid(input) && hasOnlyLettersAndDigits(input) && hasAtLeastTwoDigits(input)) {
             System.out.println("Password is valid");
         }
+    }
 
+    
+    private static boolean isLengthValid(String password) {
+        if (password.length() >= 6 && password.length() <= 10) {
+            return true;
+        } else {
+            System.out.println("Password must be between 6 and 10 characters");
+            return false;
+        }
+    }
+
+    
+    private static boolean hasOnlyLettersAndDigits(String password) {
+        for (char ch : password.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch)) {
+                System.out.println("Password must consist only of letters and digits");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    private static boolean hasAtLeastTwoDigits(String password) {
+        int digitCount = 0;
+        for (char ch : password.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                digitCount++;
+            }
+        }
+        if (digitCount >= 2) {
+            return true;
+        } else {
+            System.out.println("Password must have at least 2 digits");
+            return false;
+        }
     }
 }
-
-// Source code from Diyan Dichev
