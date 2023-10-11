85cf887a03834c713bc2b3194c36765b5b3f0046
Boris Manzov <borismanzov@outlook.com>
2023-10-11 12:21:58 +0300

    Create binaryDigitsCount.js

---
+++ Programming Fundamentals with JavaScript/10.10.2023 Common Lab/binaryDigitsCount.js
@@ -0,0 +1,17 @@
+function countBinaryDigit(n, b) {
+    let binaryRepresentation = n.toString(2);
+
+    let count = 0;
+    for(let digit of binaryRepresentation) {
+
+        if(digit == b.toString()) {
+            count++
+        }
+    }
+
+    console.log(`Binary representation of ${n} is ${binaryRepresentation}`);
+    console.log(`Digit ${b} appears ${count} times in it.`);
+
+}
+
+countBinaryDigit(20, 0);
