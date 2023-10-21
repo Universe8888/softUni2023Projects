88ff55ffa88a9581d1eda3af0b455b758f8a488c
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-21 11:17:38 +0300

    Update bunnyKill.js

--- Programming Fundamentals with JavaScript/10.13.2023 Exercise/bunnyKill.js
+++ Programming Fundamentals with JavaScript/10.13.2023 Exercise/bunnyKill.js
@@ -1,44 +1,32 @@
 function calculateBombDamage(input) {
     
     function convertToMatrix(matrixStrings) {
-        let matrix = [];
-        for (let i = 0; i < matrixStrings.length; i++) {
-            let row = [];
-            let rowStrings = matrixStrings[i].split(' ');
-            for (let j = 0; j < rowStrings.length; j++) {
-                row.push(Number(rowStrings[j]));
-            }
-            matrix.push(row);
-        }
-        return matrix;
+        return matrixStrings.map(rowString => {
+            return rowString.split(' ').map(Number);
+        });
     }
 
     function extractBombCoordinates(bombCoordString) {
-        let bombCoords = [];
-        let coords = bombCoordString.split(' ');
-        for (let i = 0; i < coords.length; i++) {
-            let coordParts = coords[i].split(',');
-            let x = Number(coordParts[0]);
-            let y = Number(coordParts[1]);
-            bombCoords.push([x, y]);
-        }
-        return bombCoords;
+        return bombCoordString.split(' ').map(coord => {
+            let coordParts = coord.split(',');
+            return [Number(coordParts[0]), Number(coordParts[1])];
+        });
     }
 
     function applyBombDamage(matrix, x, y) {
         let damage = matrix[x][y];
+        if (damage === 0) {
+            return { damage: 0, killed: false };
+        }
         matrix[x][y] = 0;
         for (let i = -1; i <= 1; i++) {
             for (let j = -1; j <= 1; j++) {
                 if (x + i >= 0 && x + i < matrix.length && y + j >= 0 && y + j < matrix[0].length) {
-                    matrix[x + i][y + j] = matrix[x + i][y + j] - damage;
-                    if (matrix[x + i][y + j] < 0) {
-                        matrix[x + i][y + j] = 0;
-                    }
+                    matrix[x + i][y + j] = Math.max(0, matrix[x + i][y + j] - damage);
                 }
             }
         }
-        return damage;
+        return { damage, killed: true };
     }
 
     function calculateRemaining(matrix) {
@@ -65,8 +53,11 @@ function calculateBombDamage(input) {
     let bunniesKilled = 0;
 
     for (let k = 0; k < bombCoords.length; k++) {
-        totalDamage += applyBombDamage(matrix, bombCoords[k][0], bombCoords[k][1]);
-        bunniesKilled++;
+        let bombResult = applyBombDamage(matrix, bombCoords[k][0], bombCoords[k][1]);
+        totalDamage += bombResult.damage;
+        if (bombResult.killed) {
+            bunniesKilled++;
+        }
     }
 
     let remaining = calculateRemaining(matrix);
@@ -79,5 +70,4 @@ function calculateBombDamage(input) {
 
 calculateBombDamage(['5 10 15 20', '10 10 10 10', '10 15 10 10', '10 10 10 10', '2,2 0,1']);
 
-
-//gives 30/100 in Judge!
+//gives 100/100 in Judge now!
