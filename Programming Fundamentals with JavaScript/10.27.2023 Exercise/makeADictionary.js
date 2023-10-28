003c4dcab2e57e2c068d82c949e62a1aa9cc45f4
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-28 14:34:27 +0300

    Create makeADictionary.js

---
+++ Programming Fundamentals with JavaScript/10.27.2023 Exercise/makeADictionary.js
@@ -0,0 +1,26 @@
+function makeDictionary(arr) {
+    let dictionary = {};
+
+    for (let jsonStr of arr) {
+        let parsedObj = JSON.parse(jsonStr);
+
+        for (let [key, value] of Object.entries(parsedObj)) {
+            dictionary[key] = value;
+        }
+    }
+
+    let sortedKeys = Object.keys(dictionary).sort();
+    for (let key of sortedKeys) {
+        console.log(`Term: ${key} => Definition: ${dictionary[key]}`);
+    }
+}
+
+const input = [
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+];
+
+makeDictionary(input);
