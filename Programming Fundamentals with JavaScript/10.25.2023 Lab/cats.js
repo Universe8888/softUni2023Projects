fa6558fe28eb6d5bee5a1c942d244863e1a7c736
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-28 13:30:48 +0300

    Create cats.js

---
+++ Programming Fundamentals with JavaScript/10.25.2023 Lab/cats.js
@@ -0,0 +1,23 @@
+function createCats(catData) {
+    
+    class Cat {
+        constructor(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (let data of catData) {
+        let [name, age] = data.split(' ');
+        age = Number(age);
+        const cat = new Cat(name, age);
+        cat.meow();
+    }
+}
+
+createCats(['Mellow 2', 'Tom 5']);
+createCats(['Candy 1', 'Poppy 3', 'Nyx 2']);
