57db56189f79408c3aa674ba8d3fb311cb55acae
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-10-28 12:39:49 +0300

    Create personInfo.js

---
+++ Programming Fundamentals with JavaScript/10.25.2023 Lab/personInfo.js
@@ -0,0 +1,10 @@
+function info(firstName, lastName, age) {
+    return{
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
+}
+
+const result = info("Peter", "Pan", "20");
+console.log(result);
