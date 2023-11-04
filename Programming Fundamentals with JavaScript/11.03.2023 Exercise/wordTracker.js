14fed4b693610c25d395384003b506294f59e729
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-11-04 11:08:49 +0200

    Update wordTracker.js

--- Programming Fundamentals with JavaScript/11.03.2023 Exercise/wordTracker.js
+++ Programming Fundamentals with JavaScript/11.03.2023 Exercise/wordTracker.js
@@ -1,22 +1,65 @@
-function wordsTracker(inputArray) {
-    let wordsToFind = inputArray.shift().split(' ');
-    let wordCounts = {};
+// function wordsTracker(inputArray) {
+//     let wordsToFind = inputArray.shift().split(' ');
+//     let wordCounts = {};
 
-    for (let word of wordsToFind) {
+//     for (let word of wordsToFind) {
+//         wordCounts[word] = 0;
+//     }
+
+//     for (let word of inputArray) {
+//         if (word in wordCounts) {
+//             wordCounts[word]++;
+//         }
+//     }
+
+//     let entries = Object.entries(wordCounts).sort((a, b) => b[1] - a[1]);
+
+//     for (let [word, count] of entries) {
+//             console.log(`${word} - ${count}`);
+//     }
+// }
+
+// wordsTracker([
+//   'this sentence',
+//   'In', 'this', 'sentence', 'you', 'have',
+//   'to', 'count', 'the', 'occurrences', 'of',
+//   'the', 'words', 'this', 'and', 'sentence',
+//   'because', 'this', 'is', 'your', 'task'
+// ]);
+
+// wordsTracker([
+//   'is the',
+//   'first', 'sentence', 'Here', 'is',
+//   'another', 'the', 'And', 'finally', 'the',
+//   'the', 'sentence'
+// ]);
+
+
+// Alexandra's solve gives 100/100
+
+function wordsTracker(inputArray){
+
+    const wordsToFind = inputArray[0].split(' ');
+
+    const wordCounts = {};
+
+    wordsToFind.forEach(word => {
         wordCounts[word] = 0;
-    }
+      });
 
-    for (let word of inputArray) {
-        if (word in wordCounts) {
+    for (let i = 1; i < inputArray.length; i++) {
+        let word = inputArray[i];
+        if (wordCounts.hasOwnProperty(word)) {
             wordCounts[word]++;
-        }
-    }
+          }
+      }
 
-    let entries = Object.entries(wordCounts).sort((a, b) => b[1] - a[1]);
+    const sortedWordCounts = Object.entries(wordCounts)
+      .sort((a, b) => b[1] - a[1]);
 
-    for (let [word, count] of entries) {
-            console.log(`${word} - ${count}`);
-    }
+    sortedWordCounts.forEach(([word, count]) => {
+        console.log(`${word} - ${count}`);
+      });
 }
 
 wordsTracker([
@@ -27,9 +70,4 @@ wordsTracker([
   'because', 'this', 'is', 'your', 'task'
 ]);
 
-// wordsTracker([
-//   'is the',
-//   'first', 'sentence', 'Here', 'is',
-//   'another', 'the', 'And', 'finally', 'the',
-//   'the', 'sentence'
-// ]);
+// My solution gives 100/100 now. 
