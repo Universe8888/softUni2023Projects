49e8ebb345fcc02f2a53e737b5c28b3cbb68cb6f
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-11-04 11:18:21 +0200

    Create piccolo.js

---
+++ Programming Fundamentals with JavaScript/11.03.2023 Exercise/piccolo.js
@@ -0,0 +1,43 @@
+function manageParkingLot(operations) {
+    const parkingLot = new Set();
+    
+    for (const operation of operations) {
+        const [direction, carNumber] = operation.split(', ');
+
+        if (direction === 'IN') {
+            parkingLot.add(carNumber);
+        } else if (direction === 'OUT') {
+            parkingLot.delete(carNumber);
+        }
+    }
+
+    const sortedCars = Array.from(parkingLot).sort();
+
+    if (sortedCars.length === 0) {
+        console.log("Parking Lot is Empty");
+    } else {
+        console.log(sortedCars.join('\n'));
+    }
+}
+const input1 = [
+    'IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'IN, CA9999TT',
+    'IN, CA2866HI',
+    'OUT, CA1234TA',
+    'IN, CA2844AA',
+    'OUT, CA2866HI',
+    'IN, CA9876HH',
+    'IN, CA2822UU'
+  ];
+  
+  const input2 = [
+    'IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'OUT, CA1234TA'
+  ];
+  
+  manageParkingLot(input1);
+//   manageParkingLot(input2);
