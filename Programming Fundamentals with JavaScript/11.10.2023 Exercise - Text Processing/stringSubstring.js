531f11f794488cc35116132c8c9aa8224a6c850f
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-11-12 11:48:12 +0200

    Update stringSubstring.js

--- Programming Fundamentals with JavaScript/11.10.2023 Exercise - Text Processing/stringSubstring.js
+++ Programming Fundamentals with JavaScript/11.10.2023 Exercise - Text Processing/stringSubstring.js
@@ -14,3 +14,25 @@ function findWord(word, text) {
 findWord('javascript', 'JavaScript is the best programming language');
 
 // 80 / 100
+
+
+// //function solve(word, text) {
+//     const isFound = () => {
+//         for (const currWord of text) {
+//             if (word === currWord) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     word = word.toLowerCase();
+//     text = text.toLowerCase().split(" ");
+
+
+//     console.log(isFound() ? word : `${word} not found!`);
+// }
+
+//100/100 Tamer 
+
+//Judge searches test cases so my method wont pass without a test.
