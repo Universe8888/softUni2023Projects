dcd678756290b10384d539e970e10494012932d5
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-12-13 20:44:25 +0200

    update

--- Programming Fundamentals with JavaScript/11.29.2023 Basic Web Project/models/Contact.js
+++ Programming Fundamentals with JavaScript/11.29.2023 Basic Web Project/models/Contact.js
@@ -1,1 +1,10 @@
 // TODO: Create Contact model and export it
+
+class Contact {
+    constructor(name, numeber) {
+        this.name = name;
+        this.number = numeber;   
+    }
+}
+
+module.exports = Contact;
