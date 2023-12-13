00677554526c1725a70f58e30769d684466a5ad7
Boris Manzov <125819932+Universe8888@users.noreply.github.com>
2023-12-13 20:44:29 +0200

    Update phonebook.js

--- Programming Fundamentals with JavaScript/11.29.2023 Basic Web Project/phonebook.js
+++ Programming Fundamentals with JavaScript/11.29.2023 Basic Web Project/phonebook.js
@@ -3,3 +3,18 @@
 	add methods for adding in the phonebook and getting it
 	export the methods
 */
+
+var phonebook = [];
+
+function getPhonebook() {
+	return phonebook;
+}
+
+function addContact(contact) {
+	phonebook.push(contact);
+}
+
+module.exports = {
+	getPhonebook,
+	addContact
+};
